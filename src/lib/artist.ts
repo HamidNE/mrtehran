0f872de3e6e35a47a934170da4fee1974ed09bd3
HamidNE <hamidne@mail.ru>
2024-05-16 09:47:21 +0330

    feat: add download artist top musics

--- src/lib/artist.ts
+++ src/lib/artist.ts
@@ -1,7 +1,12 @@
-import {fetchArtistLatestSongs} from "../utils/api";
+import {fetchArtistLatestSongs, fetchArtistTopSongs} from "../utils/api";
 import {downloadSongs} from "./download";
 
 export async function downloadArtistLatestSongs(id: string, destination: string) {
     const res = await fetchArtistLatestSongs(id);
+    await downloadSongs(res, destination);
+}
+
+export async function downloadArtistTopSongs(id: string, destination: string) {
+    const res = await fetchArtistTopSongs(id);
     await downloadSongs(res, destination);
 }
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -13,6 +13,10 @@ export async function fetchArtistLatestSongs(id: string) {
     return api<Track[]>(`https://api.mrtehran.app/web/v1/artist_latest_songs?artist_id=${id}`).then(res => res.tracks);
 }
 
+export async function fetchArtistTopSongs(id: string) {
+    return api<Track[]>(`https://api.mrtehran.app/web/v1/artist_top_songs?artist_id=${id}`).then(res => res.tracks);
+}
+
 export async function fetchAlbumSongs(id: string) {
     return api<Track[]>(`https://api.mrtehran.app/web/v1/album_data?album_id=${id}`).then(res => res.tracks);
 }
