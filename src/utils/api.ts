707bf13424f982a5bb86d356a5377f4ce6e99159
HamidNE <hamidne@mail.ru>
2024-05-18 10:07:22 +0330

    feat(api): add api polling method and update api function

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,22 +1,37 @@
 import {Track} from "../types/api";
 
-async function api<T>(url: string): Promise<{ tracks: T }> {
-    return fetch(url, {
-        "headers": {
+async function api(url: string): Promise<{ tracks: Track[], end: boolean }> {
+    return fetch(new URL(url, 'https://api.mrtehran.app/web/v1/'), {
+        headers: {
             "Accept-Language": "en-US,en;q=0.5",
             "x-api-key": "E3qfMUMiF68amHBoSScO832ypA6GgfEGVNoHhuxtfPNXPSCFueR8cNX1rgcoHqdG",
         },
     }).then(res => res.json());
 }
 
+async function apiPolling(url: string) {
+    const items = [];
+    for (let i = 1;; i++) {
+        const newUrl = url.replace(/page=\d/, `page=${i}`)
+        const res = await api(newUrl);
+        items.push(...res.tracks);
+        if (res.end) break;
+    }
+    return items;
+}
+
 export async function fetchArtistLatestSongs(id: string) {
-    return api<Track[]>(`https://api.mrtehran.app/web/v1/artist_latest_songs?artist_id=${id}`).then(res => res.tracks);
+    return apiPolling(`artist_latest_songs?artist_id=${id}&page=1`);
 }
 
 export async function fetchArtistTopSongs(id: string) {
-    return api<Track[]>(`https://api.mrtehran.app/web/v1/artist_top_songs?artist_id=${id}`).then(res => res.tracks);
+    return apiPolling(`artist_top_songs?artist_id=${id}&page=1`);
 }
 
 export async function fetchAlbumSongs(id: string) {
-    return api<Track[]>(`https://api.mrtehran.app/web/v1/album_data?album_id=${id}`).then(res => res.tracks);
+    return apiPolling(`album_data?album_id=${id}&page=1`);
+}
+
+export async function fetchPlaylistSongs(id: string) {
+    return apiPolling(`playlist_data?playlist_id=${id}&sort_id=0&page=1`);
 }
